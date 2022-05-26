cf6f0dd15d28f5f7ba262804bfb6f4d846d25d53
Sete777 <owleg250@hotmail.com>
2022-05-26 09:43:55 -0300

    Sexta Atividade Python

---
+++ att006.py
@@ -0,0 +1,6 @@
+number = int(input('Digite um valor:'))
+numdobro = number * 2
+numtriplo = number * 3
+numrquadrado = number**(1/2)
+print(f'O seu número é {number}, o dobro dele é {numdobro}, o triplo {numtriplo} e a raiz quadrada dele é {numrquadrado:.3f}')
+#Utilizando **(1/2), se consegue chegar a raiz quadrada
