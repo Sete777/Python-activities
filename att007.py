2dc2037560c0b67f28ccb544871a35e5b95c13b9
Sete777 <owleg250@hotmail.com>
2022-05-27 17:19:14 -0300

    Setima Atividade Python

---
+++ att007.py
@@ -0,0 +1,6 @@
+notap = int(input('DIgite a sua primeira nota:'))
+notas = int(input('Digite a sua segunda nota:'))
+soma = notap + notas
+media = soma / 2
+print(f'A sua media é {media}')
+#A media das notas utilizando a quantidade de notas para faazer a media;
