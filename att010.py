f9ac0fab519ae4af8570ee1a62f6defc271e2ddf
Sete777 <owleg250@hotmail.com>
2022-06-06 07:50:03 -0300

    Decima Atividade Python

---
+++ att010.py
@@ -0,0 +1,4 @@
+money = float(input('Quanto dinheiro você tem na carteira atualmente?'))
+dolar = 3.27
+realpdolar = money / dolar
+print(f'Com o dinheiro que está em sua carteira que está no valor de {money}, você pode comprar {realpdolar:.2f} de dolares. Considerando a cota~]ap de R$3.27')
