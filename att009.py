d3441c7ab0496d65e9f17f611f4504f8d3f3617f
Sete777 <owleg250@hotmail.com>
2022-06-06 07:49:29 -0300

    Nona Atividade Python

---
+++ att009.py
@@ -0,0 +1,12 @@
+nq = int(input('Digite um número e verá sua tabuada. '))
+nq2 = nq * 2
+nq3 = nq * 3
+nq4 = nq * 4
+nq5 = nq * 5
+nq6 = nq * 6
+nq7 = nq * 7
+nq8 = nq * 8
+nq9 = nq * 9
+nq10 = nq * 10
+print(f'O seu número é {nq} e a tabuada está a seguir: \n {nq} * 1 = {nq} | {nq} * 2 = {nq2} | {nq} * 3 = {nq3}\n {nq} * 4 = {nq4} | {nq} * 5 = {nq5} | {nq} * 6 = {nq6}\n {nq} * 7 = {nq7} | {nq} * 8 = {nq8} | {nq} * 9 = {nq9}\n {nq:>13} * 10 = {nq10}  ')
+#Utilizando o \n ele separa o print para a próxima linha de código, utilizando o ,end=' ' se separa as linhas.
