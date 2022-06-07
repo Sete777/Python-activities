fae43d8ab61fd2c9b0de124577db25ea53a3fa96
Sete777 <owleg250@hotmail.com>
2022-06-07 14:42:17 -0300

    Decima Primeira Atividade Python

---
+++ att011.py
@@ -0,0 +1,5 @@
+largura = float(input('Digite quantos de largura tem a parede'))
+metros = float(input('Agora digite quantos metros tem de altura'))
+area = largura * metros
+tinta = area / 2
+print(f'Sabendo que um balde de tinta pinta 2 metros da área, irá necessitar de {tinta} L de tinta.')
